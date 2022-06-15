ed2c7278764efbb61a7e6462025ed67fe987c4b8
LucasMoreira1 <lucas_moreira@outlook.com>
2022-06-15 19:11:20 -0300

    Limpeza dos projetos antigos. Inicio projeto Ferramentas Scheduler

--- Python/Exportar dados excel.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-f = open("teste.txt", "w")
-f.write("teste teste")
-f.close()
---
+++ SONDA/SONDA_EMBRAER/SONDA_EMBRAER/FormInicial.cs
@@ -0,0 +1,10 @@
+namespace SONDA_EMBRAER
+{
+    public partial class FormInicial : Form
+    {
+        public FormInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
