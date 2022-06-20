f85f3c5b8900c68e5110758980354c5a4ab22d47
LucasMoreira1 <lucas_moreira@outlook.com>
2022-06-20 04:03:42 -0300

    Conexão SAP e SARA IDOC funcionando

--- SONDA/Ferramentas_Scheduler/Ferramentas_Scheduler/FormPrincipal.cs
+++ SONDA/Ferramentas_Scheduler/Ferramentas_Scheduler/FormPrincipal.cs
@@ -7,57 +7,13 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 using SAPFEWSELib;
 
 namespace Ferramentas_Scheduler
 {
     public partial class FormPrincipal : Form
     {
-        public class SAPActive
-        {
-            public static GuiApplication SapGuiApp { get; set; }
-            public static GuiConnection SapConnection { get; set; }
-            public static GuiSession SapSession { get; set; }
-
-            public static void OpenSap(string env)
-            {
-                SAPActive.SapGuiApp = new GuiApplication();
-
-                string connectString = null;
-                if (env.ToUpper().Equals("DEFAULT"))
-                {
-                    connectString = "1.0 Test ERP (DEFAULT)";
-                }
-                else
-                {
-                    connectString = env;
-                }
-                SAPActive.SapConnection = SAPActive.SapGuiApp.OpenConnection(connectString, Sync: true); //creates connection
-                SAPActive.SapSession = (GuiSession)SAPActive.SapConnection.Sessions.Item(0); //creates the Gui session off the connection you made
-            }
-            public static void loginSap(string myclient, string mylogin, string mypass, string mylang)
-            {
-                GuiTextField client = (GuiTextField)SAPActive.SapSession.ActiveWindow.FindByName("RSYST-MANDT", "GuiTextField");
-                GuiTextField login = (GuiTextField)SAPActive.SapSession.ActiveWindow.FindByName("RSYST-BNAME", "GuiTextField");
-                GuiTextField pass = (GuiTextField)SAPActive.SapSession.ActiveWindow.FindByName("RSYST-BCODE", "GuiPasswordField");
-                GuiTextField language = (GuiTextField)SAPActive.SapSession.ActiveWindow.FindByName("RSYST-LANGU", "GuiTextField");
-
-                client.SetFocus();
-                client.Text = myclient;
-                login.SetFocus();
-                login.Text = mylogin;
-                pass.SetFocus();
-                pass.Text = mypass;
-                language.SetFocus();
-                language.Text = mylang;
-
-                //Press the green checkmark button which is about the same as the enter key 
-                GuiButton btn = (GuiButton)SapSession.FindById("/app/con[0]/ses[0]/wnd[0]/tbar[0]/btn[0]");
-                btn.SetFocus();
-                btn.Press();
-            }
-
-        }
         public FormPrincipal()
         {
             InitializeComponent();
@@ -77,13 +33,79 @@ namespace Ferramentas_Scheduler
 
         private void btnArchiveDiario_Click(object sender, EventArgs e)
         {
-            AbrirSAP();
-        }
-        public void AbrirSAP(string[] args)
-        {
-            SAPActive.OpenSap("My connection string");
-            SAPActive.loginSap("10", "lucas", "lmsferr", "PT");
-            SAPActive.SapSession.StartTransaction("SM37");
+            SapROTWr.CSapROTWrapper sapROTWrapper = new SapROTWr.CSapROTWrapper();
+            object SapGuilRot = sapROTWrapper.GetROTEntry("SAPGUI");
+            object engine = SapGuilRot.GetType().InvokeMember("GetScriptingEngine", System.Reflection.BindingFlags.InvokeMethod, null, SapGuilRot, null);
+            GuiConnection connection = (engine as GuiApplication).OpenConnection("EBP - Produção Testes Integrados New Edwards");
+            GuiSession session = connection.Children.ElementAt(0) as GuiSession;
+
+            GuiTextField login = (GuiTextField)session.ActiveWindow.FindByName("RSYST-BNAME", "GuiTextField");
+            GuiTextField pass = (GuiTextField)session.ActiveWindow.FindByName("RSYST-BCODE", "GuiPasswordField");
+            GuiButton btn;
+            btn = (GuiButton)session.FindById("wnd[0]/tbar[0]/btn[0]");
+            
+            login.SetFocus(); login.Text = "schedule2";
+            pass.SetFocus(); pass.Text = "sCHeduLe2##";
+            btn.SetFocus(); btn.Press();
+
+            session.StartTransaction("Sara");
+
+            //GuiTextField sara = (GuiTextField)session.ActiveWindow.FindById("wnd[0]/tbar[0]/okcd", "sara");
+            //sara.SetFocus(); sara.Text = "SARA";
+            btn.SetFocus(); btn.Press();
+            GuiTextField idoc = (GuiTextField)session.ActiveWindow.FindById("wnd[0]/usr/ctxtARCH_TXT-OBJECT", "idoc");
+            idoc.SetFocus(); idoc.Text = "idoc";
+            GuiButton btnDelete;
+            btnDelete = (GuiButton)session.FindById("wnd[0]/usr/btnDELETE");
+            btnDelete.SetFocus(); btnDelete.Press();
+
+            GuiButton btn1;
+            btn1 = (GuiButton)session.FindById("wnd[0]/usr/btn%_AUTOTEXT009");
+            btn1.SetFocus(); btn1.Press();
+
+            GuiButton btn2;
+            btn2 = (GuiButton)session.FindById("wnd[1]/tbar[0]/btn[8]");
+            btn2.SetFocus(); btn2.Press();
+
+            GuiButton btn9;
+            btn9 = (GuiButton)session.FindById("wnd[1]/tbar[0]/btn[0]");
+            btn9.SetFocus(); btn9.Press();
+
+
+            GuiButton btn3;
+            btn3 = (GuiButton)session.FindById("wnd[0]/usr/btn%_AUTOTEXT007");
+            btn3.SetFocus(); btn3.Press();
+
+            GuiButton btn4;
+            btn4 = (GuiButton)session.FindById("wnd[1]/usr/btnSOFORT_PUSH");
+            btn4.SetFocus(); btn4.Press();
+
+            GuiButton btn5;
+            btn5 = (GuiButton)session.FindById("wnd[1]/tbar[0]/btn[11]");
+            btn5.SetFocus(); btn5.Press();
+
+            GuiButton btn6;
+            btn6 = (GuiButton)session.FindById("wnd[0]/usr/btn%_AUTOTEXT008");
+            btn6.SetFocus(); btn6.Press();
+
+            GuiTextField local = (GuiTextField)session.ActiveWindow.FindById("wnd[1]/usr/ctxtPRI_PARAMS-PDEST", "local");
+            local.SetFocus(); local.Text = "LOCAL";
+
+            GuiButton btn7;
+            btn7 = (GuiButton)session.FindById("wnd[1]/tbar[0]/btn[13]");
+            btn7.SetFocus(); btn7.Press();
+
+            GuiButton btn8;
+            btn8 = (GuiButton)session.FindById("wnd[2]/tbar[0]/btn[0]");
+            btn8.SetFocus(); btn8.Press();
+
+            GuiButton btn10;
+            btn10 = (GuiButton)session.FindById("wnd[0]/tbar[1]/btn[8]");
+            btn10.SetFocus(); btn10.Press();
+            //GuiButton btn9;
+            //btn9 = (GuiButton)session.FindById("wnd[0]/usr/btnDELETE");
+
+
         }
     }
 }
